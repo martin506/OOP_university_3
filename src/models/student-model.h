231ac70fdf7939a39bd982d66ad6f8869cefd407
Martynas Simanavicius <martynas.simanavicius2@gmail.com>
2025-04-07 09:38:58 +0300

    changed sutdent to be a class instead of a struct

--- src/models/student-model.h
+++ src/models/student-model.h
@@ -3,34 +3,68 @@
 
 #include <string>
 
-struct Student
-{
-    std::string name = "name";
-    std::string surname = "surname";
-    int examGrade = 10;
+class Student {
+private:
+    std::string name;
+    std::string surname;
+    int examGrade;
+    double finalGradeWithAverage;
+    double finalGradeWithMedian;
 
-    double finalGradeWithAverage = 0;
-    double finalGradeWithMedian = 0;
+public:
+    Student()
+        : name("name"), surname("surname"), examGrade(10), finalGradeWithAverage(0), finalGradeWithMedian(0) {}
 
-    bool isFinalGradeWithAveragePassing() const
-    {
+    Student(const std::string& name, const std::string& surname, int examGrade, double finalGradeWithAverage, double finalGradeWithMedian)
+        : name(name), surname(surname), examGrade(examGrade), finalGradeWithAverage(finalGradeWithAverage), finalGradeWithMedian(finalGradeWithMedian) {}
+
+    std::string getName() const {
+        return name;
+    }
+
+    void setName(const std::string& newName) {
+        name = newName;
+    }
+
+    std::string getSurname() const {
+        return surname;
+    }
+
+    void setSurname(const std::string& newSurname) {
+        surname = newSurname;
+    }
+
+    int getExamGrade() const {
+        return examGrade;
+    }
+
+    void setExamGrade(int newExamGrade) {
+        examGrade = newExamGrade;
+    }
+
+    double getFinalGradeWithAverage() const {
+        return finalGradeWithAverage;
+    }
+
+    void setFinalGradeWithAverage(double newFinalGradeWithAverage) {
+        finalGradeWithAverage = newFinalGradeWithAverage;
+    }
+
+    double getFinalGradeWithMedian() const {
+        return finalGradeWithMedian;
+    }
+
+    void setFinalGradeWithMedian(double newFinalGradeWithMedian) {
+        finalGradeWithMedian = newFinalGradeWithMedian;
+    }
+
+    bool isFinalGradeWithAveragePassing() const {
         return finalGradeWithAverage >= 5;
     }
 
-    bool isFinalGradeWithMedianPassing() const
-    {
+    bool isFinalGradeWithMedianPassing() const {
         return finalGradeWithMedian >= 5;
     }
-
-    Student() : name("name"), surname("surname"), examGrade(10), finalGradeWithAverage(0), finalGradeWithMedian(0) {}
-
-    Student(
-        const std::string &name,
-        const std::string &surname,
-        int examGrade,
-        double finalGradeWithAverage,
-        double finalGradeWithMedian
-    ) : name(name), surname(surname), examGrade(examGrade), finalGradeWithAverage(finalGradeWithAverage), finalGradeWithMedian(finalGradeWithMedian) {}
 };
 
 #endif
