4739be03e6769b1d507a530044368aa4a6e339ca
Martynas Simanavicius <martynas.simanavicius2@gmail.com>
2025-05-10 14:04:41 +0300

    rule of five for human class

--- src/models/abstract/human-model.h
+++ src/models/abstract/human-model.h
@@ -9,11 +9,18 @@ private:
     std::string surname;
 
 public:
-    inline std::string getName() const { return name; }
-    inline void setName(const std::string &newName) { name = newName; }
-    inline std::string getSurname() const { return surname; }
-    inline void setSurname(const std::string &newSurname) { surname = newSurname; }
+    Human();
+    Human(const std::string& name, const std::string& surname);
+    ~Human();
+    Human(const Human& other);                // Copy Constructor
+    Human& operator=(const Human& other);     // Copy Assignment Operator
+    Human(Human&& other) noexcept;            // Move Constructor
+    Human& operator=(Human&& other) noexcept; // Move Assignment Operator
 
+    inline std::string getName() const { return name; }
+    inline void setName(const std::string& newName) { name = newName; }
+    inline std::string getSurname() const { return surname; }
+    inline void setSurname(const std::string& newSurname) { surname = newSurname; }
 };
 
 #endif
