4ca48d632503be4eb2a8b40687b512f1a1df8052
Martynas Simanavicius <martynas.simanavicius2@gmail.com>
2025-05-10 14:42:15 +0300

    input output for Human class

--- src/models/abstract/human-model.h
+++ src/models/abstract/human-model.h
@@ -2,6 +2,7 @@
 #define HUMAN_MODEL_H
 
 #include <string>
+#include <iostream>
 
 class Human {
 private:
@@ -17,10 +18,28 @@ public:
     Human(Human&& other) noexcept;            // Move Constructor
     Human& operator=(Human&& other) noexcept; // Move Assignment Operator
 
+    // input operator
+    friend std::istream& operator>>(std::istream& in, Human& human) {
+        std::cout << "Enter Name: ";
+        in >> human.name;
+        std::cout << "Enter Surname: ";
+        in >> human.surname;
+        return in;
+    }
+
+    // output operator
+    friend std::ostream& operator<<(std::ostream& out, const Human& human) {
+        out << "Name: " << human.name << ", Surname: " << human.surname << "\n";
+        return out;
+    }
+
+
     inline std::string getName() const { return name; }
     inline void setName(const std::string& newName) { name = newName; }
     inline std::string getSurname() const { return surname; }
     inline void setSurname(const std::string& newSurname) { surname = newSurname; }
+
+    
 };
 
 #endif
